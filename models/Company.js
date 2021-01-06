636b4e72e3c97fcfd362cd0deaf5edc57ec0acfe
Đinh Lê Triều Dương <39703705+dinhletrieuduong@users.noreply.github.com>
2021-01-06 12:40:29 +0700

    Update Company.js

--- models/Company.js
+++ models/Company.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CompanySchema = new Schema({
-    companyName: {type: String, required: true},
-    companySize: {type: Number, required: true},
-    companyWebsite: {type: String, required: true},
-    companyEmail: {type: String, required: true},
+    companyName: {type: String, required: false},
+    companySize: {type: Number, required: false},
+    companyWebsite: {type: String, required: false},
+    companyEmail: {type: String, required: false},
     logo: {type: String, required: false},
-    address: {type: String, required: true},
-    location: {type: String, required: true},
+    address: {type: String, required: false},
+    location: {type: String, required: false},
     hotline: {type: String, required: false},
-    categories: {type: [String], required: true},
+    categories: {type: [String], required: false},
 
     isValidated: {type: Boolean, default: false},
     isBanned: {type: Boolean, default: false},
